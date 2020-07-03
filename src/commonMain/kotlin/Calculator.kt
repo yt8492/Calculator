3fc191700e32d9692ebc162fe42bc8222ab6268c
yt8492 <tmymyut2000@gmail.com>
2020-07-03 14:09:16 +0900

    implement ShuntingYard algorithm

--- src/commonMain/kotlin/Calculator.kt
+++ src/commonMain/kotlin/Calculator.kt
@@ -1,41 +1,130 @@
-import com.github.h0tk3y.betterParse.combinators.leftAssociative
-import com.github.h0tk3y.betterParse.combinators.map
-import com.github.h0tk3y.betterParse.combinators.or
-import com.github.h0tk3y.betterParse.combinators.times
-import com.github.h0tk3y.betterParse.combinators.unaryMinus
-import com.github.h0tk3y.betterParse.combinators.use
-import com.github.h0tk3y.betterParse.grammar.Grammar
-import com.github.h0tk3y.betterParse.grammar.parser
-import com.github.h0tk3y.betterParse.parser.Parser
-import kotlin.math.PI
+sealed class CalculateToken {
+    data class Num(val n: Double) : CalculateToken()
+    sealed class Operator : CalculateToken(), Comparable<Operator> {
+        object Plus : Operator()
+        object Minus : Operator()
+        object Times : Operator()
+        object Divide : Operator()
 
-sealed class AST {
-    data class Number(val n: Double) : AST()
-    data class Add(val a: AST, val b: AST) : AST()
-    data class Sub(val a: AST, val b: AST) : AST()
-    data class Mul(val a: AST, val b: AST) : AST()
-    data class Div(val a: AST, val b: AST) : AST()
+        override fun compareTo(other: Operator): Int {
+            return when (this) {
+                is Plus, is Minus -> {
+                    if (other == Plus || other == Minus) {
+                        0
+                    } else {
+                        -1
+                    }
+                }
+                is Times, is Divide -> {
+                    if (other == Times || other == Divide) {
+                        0
+                    } else {
+                        1
+                    }
+                }
+            }
+        }
+    }
+
+    sealed class Bracket : CalculateToken() {
+        object Left : Bracket()
+        object Right : Bracket()
+    }
 }
 
-object Parser : Grammar<AST>() {
-    val number by token("""-?\d+(\.\d+)?""")
-    val pi by token("""pi""")
-    val whiteSpace by token("""\s+""", ignore = true)
-    val plus by token("""\+""")
-    val minus by token("""-""")
-    val times by token("""[*×]""")
-    val div by token("""/""")
-    val leftBracket by token("""\(""")
-    val rightBracket by token("""\)""")
-
-    val bracedTerm by -leftBracket * parser(this::addSubTerm) * -rightBracket
-    val seed: Parser<AST> by number map { AST.Number(it.text.toDouble()) } or bracedTerm or pi map { AST.Number(PI) }
-    val mulDivTerm by leftAssociative(seed, times or div use { type }) { a, op, b ->
-        if (op == times) AST.Mul(a, b) else AST.Div(a, b)
+sealed class CalculateResult {
+    data class Success(val n: Double) : CalculateResult()
+    sealed class Failure : CalculateResult() {
+        object ParseError : Failure()
+        object ArithmeticError : Failure()
     }
-    val addSubTerm by leftAssociative(mulDivTerm, plus or minus use { type }) { a, op, b ->
-        if (op == plus) AST.Add(a, b) else AST.Sub(a, b)
+}
+
+private data class ShuntingYard(
+        val operatorStack: List<CalculateToken>,
+        val outputQueue: List<CalculateToken>
+)
+
+object Calculator {
+    fun calculate(
+            tokens: List<CalculateToken>
+    ): CalculateResult {
+        val shuntingYard = tokens.fold(ShuntingYard(listOf(), listOf())) { acc, token ->
+            when (token) {
+                is CalculateToken.Num -> {
+                    acc.copy(outputQueue = acc.outputQueue + token)
+                }
+                is CalculateToken.Operator -> {
+                    acc.operatorStack.takeWhile { it is CalculateToken.Operator && token <= it }
+                            .fold(acc) { a, op ->
+                                ShuntingYard(
+                                        a.operatorStack.drop(1),
+                                        a.outputQueue + op
+                                )
+                            }
+                            .let {
+                                it.copy(operatorStack = listOf(token) + it.operatorStack)
+                            }
+                }
+                is CalculateToken.Bracket.Left -> {
+                    acc.copy(operatorStack = listOf(token) + acc.operatorStack)
+                }
+                is CalculateToken.Bracket.Right -> {
+                    if (!acc.operatorStack.any { it is CalculateToken.Bracket.Left }) {
+                        return CalculateResult.Failure.ParseError
+                    }
+                    acc.operatorStack
+                            .takeWhile { it !is CalculateToken.Bracket.Left }
+                            .fold(acc) { a, op ->
+                                ShuntingYard(
+                                        a.operatorStack.drop(1),
+                                        a.outputQueue + op
+                                )
+                            }
+                }
+            }
+        }
+        val tokens = shuntingYard.outputQueue + shuntingYard.operatorStack
+        return calculateReversePolish(tokens)
     }
 
-    override val rootParser: Parser<AST> by addSubTerm
+    private fun calculateReversePolish(tokens: List<CalculateToken>): CalculateResult {
+        try {
+            var stack = listOf<Double>()
+            tokens.forEach {
+                when (it) {
+                    is CalculateToken.Num -> {
+                        stack = listOf(it.n) + stack
+                    }
+                    is CalculateToken.Operator -> {
+                        val a = stack[0]
+                        val b = stack[1]
+                        stack = stack.drop(2)
+                        when (it) {
+                            is CalculateToken.Operator.Plus -> {
+                                stack = listOf(a + b) + stack
+                            }
+                            is CalculateToken.Operator.Minus -> {
+                                stack = listOf(a - b) + stack
+                            }
+                            is CalculateToken.Operator.Times -> {
+                                stack = listOf(a * b) + stack
+                            }
+                            is CalculateToken.Operator.Divide -> {
+                                stack = listOf(a / b) + stack
+                            }
+                        }
+                    }
+                    is CalculateToken.Bracket -> {
+                        return CalculateResult.Failure.ParseError
+                    }
+                }
+            }
+            return CalculateResult.Success(stack[0])
+        } catch (e: ArithmeticException) {
+            return CalculateResult.Failure.ArithmeticError
+        } catch (e: IndexOutOfBoundsException) {
+            return CalculateResult.Failure.ParseError
+        }
+    }
 }
