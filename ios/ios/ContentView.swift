29e32c72bf20ccf2c4973c4866eec087d72677d3
yt8492 <tmymyut2000@gmail.com>
2020-07-16 03:51:14 +0900

    add ios

---
+++ ios/ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios
+//
+//  Created by megane on 2020/07/16.
+//  Copyright © 2020 yt8492. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
